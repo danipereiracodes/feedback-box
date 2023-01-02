15d6792448db5f867a961e9463be403554c0e58f
daniel.pereira <daniel.pereira@0DPEREIRAXL5520.babel-si.local>
2023-01-02 17:35:13 +0100

    deleted framer motion

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import FeedbackContext from "../context/FeedbackContext"
 import FeedbackItem from "./FeedbackItem"
-import {motion, AnimatePresence} from 'framer-motion'
+
 
 
 function FeedbackList() {
@@ -11,18 +11,13 @@ function FeedbackList() {
     }
   return (
     <div className="feedback-list">
-        <AnimatePresence>
+        
         {feedback.map((item) => (
-            <motion.div 
-            key={item.id}
-            initial={{opacity:0}}
-            animate={{opacity:1}}
-            exit={{opacity:0}}
-            >
+            
             <FeedbackItem key={item.id} item={item}/>
-            </motion.div>
+            
         ))}
-        </AnimatePresence>
+        
     </div>
   )
 }
